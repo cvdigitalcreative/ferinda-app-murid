19b928d2e03f2df0d25f1d800be9d80ecb2e0fba
vandoc <ichvandiocta@gmail.com>
2020-08-16 16:29:13 +0700

    Add home activity for bottom navigation container

---
+++ app/src/main/java/com/digitalcreative/appmurid/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.digitalcreative.appmurid.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.digitalcreative.appmurid.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
