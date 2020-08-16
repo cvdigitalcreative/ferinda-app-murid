302b7df9fb8f6f908f11f0fa3fffee39d5ed62bc
vandoc <ichvandiocta@gmail.com>
2020-08-16 16:12:21 +0700

    Ignore .idea

--- app/src/main/java/com/digitalcreative/appmurid/LoginActivity.kt
+++ app/src/main/java/com/digitalcreative/appmurid/LoginActivity.kt
@@ -1,11 +1,11 @@
 package com.digitalcreative.appmurid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
